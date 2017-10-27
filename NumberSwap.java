24c25eec8534bd3c751b28e64ed9aecb60ca7928
gravydave <heracuo@gmail.com>
2017-10-27 13:48:54 +0800

    Added incomplete NumberSwap algorithm

---
+++ NumberSwap.java
@@ -0,0 +1,20 @@
+//A quick number swapping algorithm to test pushing and committing in Git
+
+import java.util.*;
+
+public class NumberSwap
+{
+	public static void main(String[] args)
+	{
+		int x = 5;
+		int y = 9;
+
+		System.out.println("X equals = " + x);
+		System.out.println("Y equals = " + y);
+		
+		//Swap algorithm goes here
+
+	}
+
+}
+	
